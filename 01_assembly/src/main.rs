a56fe07ef53cdd28b0c0c93ab63abfd9d19d9a4f
Zsolt Krüpl <hg2ecz@ham.hu>
2022-04-08 17:37:24 +0200

    assembly in stable Rust

--- 01_assembly/src/main.rs
+++ 01_assembly/src/main.rs
@@ -1,7 +1,7 @@
-#![feature(asm)] // asm: nightly build
 #![no_std] // don't link the Rust standard library
 #![no_main] // disable all Rust-level entry points
 
+use core::arch::asm;
 use core::panic::PanicInfo;
 
 /// This function is called on panic.
